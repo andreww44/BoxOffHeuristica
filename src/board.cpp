78505ab729e404983456bb2516c7b59ec728dc88
andreww44 <monsalve.andres.salas@gmail.com>
2024-10-06 16:31:31 -0300

    CLION

---
+++ src/board.cpp
@@ -0,0 +1,5 @@
+//
+// Created by monsa on 06/10/2024.
+//
+
+#include "board.h"
---
+++ src/board.h
@@ -0,0 +1,16 @@
+//
+// Created by monsa on 06/10/2024.
+//
+
+#ifndef BOARD_H
+#define BOARD_H
+
+
+
+class board {
+    
+};
+
+
+
+#endif //BOARD_H
